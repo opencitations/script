7db1441abd953002fcfa0cb3809af4998f362bb1
Silvio Peroni <essepuntato@gmail.com>
2018-08-29 22:41:29 +0200

    Updated conf file

--- spacin/conf.py
+++ spacin/conf.py
@@ -16,26 +16,55 @@
 
 __author__ = 'essepuntato'
 
-# Configuration for local test
-base_dir = "./test/data/corpus/"
+# Official configuration
+base_dir = "/srv/data/corpus/"
 base_iri = "https://w3id.org/oc/corpus/"
 triplestore_url = "http://localhost:3000/blazegraph/sparql"
 triplestore_url_real = "https://w3id.org/oc/sparql"
 context_path = "https://w3id.org/oc/corpus/context.json"
-context_file_path = "./corpus/context.json"
-info_dir = "./test/share/id-counter/"
-temp_dir_for_rdf_loading = "./test/"
-orcid_conf_path = "./script/spacin/orcid_conf.json"
-reference_dir = "./test/share/ref/todo/"
-reference_dir_error = "./test/index/ref/err/"
-reference_dir_done = "./test/index/ref/done/"
+context_file_path = "/srv/data/corpus/context.json"
+info_dir = "/srv/share/id-counter/"
+temp_dir_for_rdf_loading = "/tmp/"
+orcid_conf_path = "/srv/dev/script/spacin/orcid_conf.json"
+reference_dir = "/srv/share/ref/todo/"
+reference_dir_error = "/srv/index/ref/err/"
+reference_dir_done = "/srv/index/ref/done/"
 dataset_home = "http://opencitations.net/"
-dir_split_number = 6  # This must be multiple of the following one
-items_per_file = 3
+dir_split_number = 10000  # This must be multiple of the following one
+items_per_file = 1000
 default_dir = "_"
 supplier_dir = {
-    "1": "01110"
+    "101": "01110",
+    "102": "01120",
+    "103": "01130",
+    "104": "01140",
+    "105": "01150",
+    "106": "01160",
+    "107": "01170",
+    "108": "01180",
+    "109": "01190",
+    "110": "01910",
+    "111": "01210",
+    "112": "01220",
+    "113": "01230",
+    "114": "01240",
+    "115": "01250",
+    "116": "01260",
+    "117": "01270",
+    "118": "01280",
+    "119": "01290",
+    "120": "01920",
+    "121": "01310",
+    "122": "01320",
+    "123": "01330",
+    "124": "01340",
+    "125": "01350",
+    "126": "01360",
+    "127": "01370",
+    "128": "01380",
+    "129": "01390",
+    "130": "01930",
 }
-interface = "lo0"
-do_parallel = False
-sharing_dir = "./test/share/data/"
+interface = "eth0"
+do_parallel = True
+sharing_dir = "/srv/share/data/"
