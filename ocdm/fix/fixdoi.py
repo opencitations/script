b166c62997f7354100618a7233ca59c3f90ac357
Silvio Peroni <essepuntato@gmail.com>
2018-08-30 22:21:26 +0200

    Another test

--- ocdm/fix/fixdoi.py
+++ ocdm/fix/fixdoi.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
             br_entity = update_br.add_br(agent_name, res=br)
             br_entity.add_triples(has_identifier_statements)
 
-            print("Graph dimensions:", len(update_br.graphs()), len(remove_id.graphs()))
+            print("Graph dimensions:", update_br.graphs()[0].triples((None, None, None)), remove_id.graphs()[0].triples((None, None, None)))
             exit(0)
 
         print("Update brs")
